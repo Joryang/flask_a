298cdf1174c1e69b52de32dc835c3f5f168ae3e5
zhao <45147851@qq.com>
2016-02-26 15:54:09 +0800

    To database

---
+++ hello.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess'
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,
+    'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+	old_name = session.get('name')
+	if old_name is not None and old_name != form.name.data:
+	    flash('Looks like you have changed your name!')
+	session['name'] = form.name.data
+	form.name.data = ''
+	return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    manager.run()
